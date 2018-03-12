cf83e24aa4dfedbe7de7adcd88c8654f1551a269
StevenS125 <sts12589@gmail.com>
2018-03-12 16:25:11 -0400

    finished securing api

---
+++ secure/client/src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+    domain: 'snydz.auth0.com',
+    clientId: 'yP9l3OTth3PQPZ1HFGC0KLP3FNJeq6R6',
+    callbackUrl: 'http://localhost:3000/callback',
+    apiUrl: 'https://localhost:4000/courses'
+  }
